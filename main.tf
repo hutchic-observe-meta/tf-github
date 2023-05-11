4b507b2d69099f025f9053dcc30e0ac6117279b9
Colin Hutchinson <colin.hutchinson@observeinc.com>
2023-05-11 11:17:45 -0400

    feat(github): demonstrate using terraform to manage github teams

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+}
+
+output "hello_world" {
+  value = "Hello, World!"
+}
